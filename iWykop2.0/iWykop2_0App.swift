da92c74a31267e49964ab6b5e8140a774df91605
Marcin Mierzejewski <marcinmierzejewski1024@gmail.com>
2023-01-27 21:30:53 +0100

    Initial Commit

---
+++ iWykop2.0/iWykop2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  iWykop2_0App.swift
+//  iWykop2.0
+//
+//  Created by Marcin Mierzejewski on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iWykop2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
